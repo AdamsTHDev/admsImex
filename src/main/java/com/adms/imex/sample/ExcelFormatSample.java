67b030392d13c418787eb0522b2d02735eaac270
kampon8 <kampon8@gmail.com>
2014-09-23 15:17:31 +0700

    Update example.

--- src/main/java/com/adms/imex/sample/ExcelFormatSample.java
+++ src/main/java/com/adms/imex/sample/ExcelFormatSample.java
@@ -4,6 +4,7 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLClassLoader;
+import java.util.List;
 
 import com.adms.imex.excelformat.DataHolder;
 import com.adms.imex.excelformat.ExcelFormat;
@@ -17,42 +18,48 @@ public class ExcelFormatSample {
 		InputStream sampleReport = URLClassLoader.getSystemResourceAsStream("sampleReport.xlsx");
 
 		ExcelFormat ex = new ExcelFormat(fileFormat);
-		DataHolder dataHolder = ex.readExcel(sampleReport);
+		DataHolder fileDataHolder = ex.readExcel(sampleReport);
 
-		System.out.println(dataHolder.get("Sheet1").get("reportAsOf").getValue());
-		System.out.println(dataHolder.get("Sheet1").get("testDateString").getValue());
-		System.out.println(dataHolder.get("Sheet1").getDataList("personList").size());
-
-		for (DataHolder person : dataHolder.get("Sheet1").getDataList("personList"))
+		List<String> sheetNames = fileDataHolder.getKeyList();
+		for (String sheetName : sheetNames)
 		{
-			System.out.println(person.get("index").getValue() + ", " + person.get("firstName").getValue() + ", " + person.get("total").getValue());
+			DataHolder sheetDataHolder = fileDataHolder.get(sheetName);
+
+			System.out.println(sheetDataHolder.get("reportAsOf").getValue());
+			System.out.println(sheetDataHolder.get("testDateString").getValue());
+			
+			System.out.println(sheetDataHolder.getDataList("personList").size());
+
+			List<DataHolder> personList = sheetDataHolder.getDataList("personList");
+			for (DataHolder person : personList)
+			{
+				System.out.println(person.printValues());
+			}
 		}
 
 		fileFormat.close();
 		sampleReport.close();
 
 		OutputStream sampleOutput = new FileOutputStream("D:/testOutput.xlsx");
-		ex.writeExcel(sampleOutput, dataHolder);
+		ex.writeExcel(sampleOutput, fileDataHolder);
 		sampleOutput.close();
 	}
-	
-	public void testGitConflic() {
-		
+
+	public void testGitConflic()
+	{
+
 		System.out.println("Hi, World!");
-		
+
 		System.out.println("Bla Bla");
-		
-		
+
 		/*
 		 * 
 		 * 
 		 * 
 		 * 
-		 * Moth Pc
-		 * Test Conflix
-		 * DiDi
+		 * Moth Pc Test Conflix DiDi
 		 */
-		
+
 		System.out.println("wow");
 	}
 
